a68ec2ed53369eb059bff398a2c57ae82147c290
Rahul Kumar <54908639+Purpledevil27@users.noreply.github.com>
2022-08-28 17:24:30 +0530

    Add procfile and update ports

--- app.js
+++ app.js
@@ -267,7 +267,11 @@ app.post("/register", function (req, res) {
     // });
 })
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 3000;
+}
 
-app.listen(3000, function () {
-    console.log("Server started on Port 3000")
+app.listen(port, function () {
+    console.log("Server has started successfully.")
 });
